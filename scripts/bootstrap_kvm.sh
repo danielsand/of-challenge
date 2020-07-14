c4d9039e5f3598f52cd4f5502186025afda94bab
Daniel Sand <daniel.sand@gmail.com>
2020-07-14 08:25:34 +0200

    60 seconds wait should be enough

--- scripts/bootstrap_kvm.sh
+++ scripts/bootstrap_kvm.sh
@@ -5,8 +5,8 @@
 minikube start --vm driver kvm2
 
 # also lets wait a momement to let minikube settle
-echo "* will sleep for 120 seconds to give k8s minikube time to settle"
-sleep 120
+echo "* will sleep for 60 seconds to give k8s minikube time to settle"
+sleep 60
 
 # enable minikube ingress
 
